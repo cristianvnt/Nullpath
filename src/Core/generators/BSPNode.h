4f21b060c1c1bf92058aab943b84e7b8d7edbf2d
DESKTOP-028M427\crist <mematoru@gmail.com>
2025-06-02 12:03:53 +0300

    Changed comments

--- src/Core/generators/BSPNode.h
+++ src/Core/generators/BSPNode.h
@@ -17,48 +17,25 @@ private:
 
 	bool canSplit(int minSizePx) const;
 
-	/*
-	* Determine split orientation
-	* @param canH true if horizontal split possible
-	* @param canV true if vertical split possible
-	* @param ratio max allowed ratio between partitions
-	* @return true for horizontal, false for vertical
-	*/
+	// Determine split orientation
 	bool decideOrientation(bool canSplitH, bool canSplitV, float splitRatio, float width, float height);
 
-	/*
-	* Decide orientation and offset for partition respecting ratio
-	* @param bounds Region to split
-    * @param minSizePx Minimum size of each partition
-    * @param ratio Max allowed ratio between partitions (ex: 1.25)
-    * @param outHorizontal true if horizontal split, false if vertical
-    * @param outOffset Distance from top/left for split line
-    * @return true if a valid split can be made
-	*/
+	// Decide orientation and offset for partition respecting ratio
 	bool choosePartition(const sf::FloatRect& bounds, int minSizePx, float splitRatio, bool& outHorizontal, float& outOffset);
 	void carveRoom(int minRoomSizePx, int paddingPx);
 	void carveBetween(const sf::Vector2f& a, const sf::Vector2f& b);
 
-	/*
-	* Recursively find a leaf node that contains a valid room
-	*/
+	// Recursively find a leaf node that contains a valid room
 	const BSPNode* getRoomNode() const;
 
 public:
 	explicit BSPNode(const sf::FloatRect& area);
 
-	/*
-	* Split this node's region into two subregions using BSP.
-	* @param minSizePx Minimum size in pixels for any subregion
-	* @param ratio Maximum allowed ratio between subregion sizes
-	* @return true if split succeeded, false otherwise
-	*/
+	// Split this node's region into two subregions using BSP.
 	void Split(int minSizePx, float splitRatio = 1.25f, int depth = 0);
 	void GenerateRooms(int minRoomSizePx, int paddingPx = 5);
 
-	/*
-	* Generate corridors for this node (recursively calls children)
-	*/
+	// Generate corridors for this node (recursively calls children)
 	void CreateCorridors();
 
 	static sf::Vector2f CenterRect(const sf::FloatRect& rect);
